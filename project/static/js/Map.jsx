0b938639708da0ccbb3c43d7a66a36a1c2b03112
roycebranning <royce5branning@yahoo.com>
2017-12-05 22:46:54 -0500

    added Map component

---
+++ project/static/js/Map.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class About extends Component {
+  constructor(props) {
+    super(props); 
+    console.log(props)
+  } 
+
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Map</h1>
+          <p></p>
+        </div>
+      </div>
+    );
+  }
+}
