801fc4491ad0d4b98f071f3a0a9467b9355f21b8
Nathan Edward Klair <nklair@dsg1.crc.nd.edu>
2017-12-13 20:18:11 -0500

    wired up api endpoints

--- project/server/api/iceform.py
+++ project/server/api/iceform.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, session, request, Blueprint, jsonify
 from project.server.database_controller import DatabaseController
+from flask import session
 
 iceform_api = Blueprint('iceform', __name__, static_folder="../../static/dist", template_folder="../../static")
 
 @iceform_api.route("/resident_info/<netid>", methods=['GET'])
 def getUserIceformData(netid):
     dc = DatabaseController.get_instance()
-    
+    info = dc.get_resident_info(netid)
+    return jsonify({"result":"success", "data":info})
+
+
 
 @iceform_api.route("/resident_info", methods=['GET'])
-def getCurrentUserIceformData(netid):
-    return "some data"
+def getCurrentUserIceformData():
+    dc = DatabaseController.get_instance()
+    info = dc.get_resident_info(session['username'])
+    return jsonify({"result":"success", "data":info})
 
 
 @iceform_api.route("/update_account", methods=['POST'])
